fb04499503a777b2a8ecd132ae2ecbd331a57206
adilasainu <adilasainudeen@gmail.com>
2023-05-25 23:27:50 +0530

    first commit

---
+++ my-app/src/components/Dropdown.svelte
@@ -0,0 +1,25 @@
+<!-- Dropdown.svelte -->
+<script>
+    let selectedOption = "";
+    
+    function handleChange(event) {
+      selectedOption = event.target.value;
+    }
+  </script>
+  
+  <main>
+    <h1>Jobs</h1>
+    
+    <label for="dropdown">Select an option:</label>
+    <select id="dropdown" on:change={handleChange}>
+      <option value="">Select an option</option>
+      <option value="option1">SOFTWARE DEVELOPER</option>
+      <option value="option2">SOFTWARE ASSISSTANT</option>
+      <option value="option3">SOFTWARE ENGINEER</option>
+    </select>
+    
+    {#if selectedOption}
+      <p>You selected: {selectedOption}</p>
+    {/if}
+  </main>
+  
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,65 @@
+
+<!-- src/routes/index.svelte -->
+<script>
+    import Dropdown from '../components/Dropdown.svelte';
+  
+    let title = 'NOTIFICATION';
+  
+    let selectedBatch = "";
+    let isCheckboxChecked = false;
+  
+    function selectBatch(event) {
+      selectedBatch = event.target.value;
+    }
+  
+    function toggleCheckbox() {
+      isCheckboxChecked = !isCheckboxChecked;
+    }
+  
+    function saveDetails() {
+      // Add logic to save the details
+      
+      // Show "SAVED" message
+      alert("SAVED");
+    }
+  </script>
+  
+  <main>
+    <h1>{title}</h1>
+    <p>It is to notify the students about the jobs</p>
+  
+    <Dropdown id="jobName" label="Select a job name" />
+  
+    <select id="batch" on:change={selectBatch}>
+      <option value="">Select a batch</option>
+      <option value="batch1">Batch 1</option>
+      <option value="batch2">Batch 2</option>
+      <option value="batch3">Batch 3</option>
+    </select>
+  
+    {#if selectedBatch}
+      <p>Selected batch: {selectedBatch}</p>
+      <a href="mailto:imca-221@scmsgroup.org?subject=JOB DRIVE">Send Email</a>
+  
+      <br />
+  
+      <label>
+        <input type="checkbox" checked={isCheckboxChecked} on:change={toggleCheckbox} />
+        I HAVE NOTIFIED  THE BATCHES
+      </label>
+  
+      {#if isCheckboxChecked}
+        <p>NOTIFIED!</p>
+      {/if}
+  
+      <button on:click={saveDetails}>Save</button>
+    {/if}
+  </main>
+  
+  <style>
+    main {
+      text-align: center;
+      margin: 20px auto;
+    }
+  </style>
+  
